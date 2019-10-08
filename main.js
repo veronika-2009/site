39d40597d850396a46ec62f82ee9dc0ee2d1abb1
Veronika Gordievich <felizfeliz0808@gmail.com>
2019-10-09 00:52:26 +0300

    pharm

--- main.js
+++ main.js
@@ -32,21 +32,68 @@ window.onclick = function(event){
 }
 
 
-let turn = [];
-//let man = document.getElementById('user2');
-let mimg = document.createElement('img');
-mimg.setAttribute('src', 'man2.png');
+
 
 
 let turn_patientConsult = document.getElementById('btn__patient-consult');
 let turn_patientMedicine = document.getElementById('btn__patient-medicine');
 let turn_patientTechnics = document.getElementById('btn__patient-technics');
 
+let turn = [];
+
 turn_patientConsult.onclick = function(){
+	let mimg = document.createElement('img');
+    mimg.setAttribute('src', 'man2.png');
 	turn.push(mimg);
-	document.body.appendChild(turn[0]);
+	document.getElementById('pacient1').appendChild(mimg);
 	
+}
+let turnMedicine = [];
+
+turn_patientMedicine.onclick = function(){
+	let img2 = document.createElement('img');
+    img2.setAttribute('src', 'girl1.png');
+	turnMedicine.push(img2);
+	document.getElementById('pacient2').appendChild(img2);
+}
+
+let turnTechnics = [];
+
+turn_patientTechnics.onclick = function(){
+	let img3 = document.createElement('img');
+    img3.setAttribute('src', 'girl.png');
+	turnTechnics.push(img3);
+	document.getElementById('pacient3').appendChild(img3);
+}
+
+
+let turn_pharmacistConsult = document.getElementById('btn__pharmacist-consult');
+let turn_ppharmacistMedicine = document.getElementById('btn__pharmacist-medicine');
+let turn_pharmacistTechnics = document.getElementById('btn__pharmacist-technics');
+
+
+
+turn_pharmacistConsult.onclick = function(){
+	let served = document.createElement('img');
+    served.setAttribute('src', 'man2.png');
+	turn.shift(served);
+	document.getElementById('pacient1').remove(served);
 }
 
 
 
+turn_ppharmacistMedicine.onclick = function(){
+	let servedMedicine = document.createElement('img');
+    servedMedicine.setAttribute('src', 'girl1.png');
+	turnMedicine.shift(servedMedicine);
+	document.getElementById('pacient2').remove(servedMedicine);
+}
+
+
+
+turn_pharmacistTechnics.onclick = function(){
+	let servedTechnics = document.createElement('img');
+    servedTechnics.setAttribute('src', 'girl.png');
+	turnTechnics.shift(servedTechnics);
+	document.getElementById('pacient3').remove(servedTechnics);
+}
